9a950aca7d67edc06c7af7ac35a768fba1de07a6
imethw <165758092+imethw@users.noreply.github.com>
2024-05-04 21:58:24 +0530

    Update Codes.py

--- Codes.py
+++ Codes.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-t.set_page_config(
-    page_title="Global Superstore"
-)
+
 #importing the dataset
 df = pd.read_excel(cleaned_dataset.xlsx,engine='openpyxl')
