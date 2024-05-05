0bccdaf53aed29b6a3e6add8c6e4e3baa0eef51f
imethw <165758092+imethw@users.noreply.github.com>
2024-05-05 15:18:00 +0530

    Update Codes.py

--- Codes.py
+++ Codes.py
@@ -49,3 +49,20 @@ charts_info = [
     {"type": "area", "x": "Market", "y": "Profit", "title": "Area Chart"},
     {"type": "density_heatmap", "x": "Country", "y": "Sales", "title": "Heatmap of Top 10 Countries in Sales", "color_scale": "reds"}
 ]
+for info in charts_info:
+    if "type" in info:
+        if info["type"] == "density_heatmap":
+            sales_by_country = sales_data.groupby('Country')['Sales'].sum().reset_index()
+            top_10_countries = sales_by_country.nlargest(10, 'Sales')
+            df_top_10_countries = sales_data[sales_data['Country'].isin(top_10_countries['Country'])]
+            fig = getattr(px, info["type"])(df_top_10_countries, x=info.get("x", None), y=info.get("y", None), title=info.get("title", None), color_continuous_scale=info.get("color_scale", None))
+        elif info["type"] == "pie":
+            fig = getattr(px, info["type"])(sales_data, names=info.get("names", None), title=info.get("title", None), hole=info.get("hole", 0.5))
+        elif info["type"] == "histogram":
+            fig = getattr(px, info["type"])(sales_data, x=info.get("x", None), title=info.get("title", None))
+        else:
+            fig = getattr(px, info["type"])(sales_data, x=info.get("x", None), y=info.get("y", None), title=info.get("title", None))
+        st.plotly_chart(fig, use_container_width=True)
+    else:
+        st.write("Invalid chart info: ", info)
+
