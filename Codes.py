118155bffb1c2c644a3de791cfc24234e7231c50
imethw <165758092+imethw@users.noreply.github.com>
2024-05-05 15:27:08 +0530

    Update Codes.py

--- Codes.py
+++ Codes.py
@@ -45,7 +45,7 @@ charts_info = [
     {"type": "box", "x": "Sub-Category", "y": "Quantity", "title": "Box Plot", "color": "green"},
     {"type": "bar", "x": "Ship Mode", "y": "Shipping Cost", "title": "Bar Chart", "color": "#eba434"},
     {"type": "pie", "names": "Order Priority", "title": "Donut Chart", "hole": 0.5},
-    {"type": "histogram", "x": "Sales", "title": "Histogram"},  # Removed color parameter
+    {"type": "histogram", "x": "Region", "title": "Histogram"},  # Removed color parameter
     {"type": "area", "x": "Market", "y": "Profit", "title": "Area Chart"},
     {"type": "density_heatmap", "x": "Country", "y": "Sales", "title": "Heatmap of Top 10 Countries in Sales", "color_scale": "reds"}
 ]
