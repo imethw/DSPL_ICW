eeef3d4d11691cc493702ccc6fa84bb6c0612f25
imethw <165758092+imethw@users.noreply.github.com>
2024-07-09 23:06:31 +0530

    Update Codes.py

--- Codes.py
+++ Codes.py
@@ -72,7 +72,7 @@ while index < len(charts_info):
             fig = getattr(px, info["type"])(sales_data, x=info.get("x", None), y=info.get("y", None), title=info.get("title", None), color_discrete_sequence=["purple"])
         elif info["type"] == "scatter":
             fig = getattr(px, info["type"])(sales_data, x=info.get("x", None), y=info.get("y", None), title=info.get("title", None), color_discrete_sequence=["red"])
-        else:
+        else info["type"] == "bar":
             fig = getattr(px, info["type"])(sales_data, x=info.get("x", None), y=info.get("y", None), title=info.get("title", None), color_discrete_sequence=["blue"])
         st.plotly_chart(fig, use_container_width=True)
     except Exception as e:
